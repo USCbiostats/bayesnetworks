ab595a43b35ed525c5f0d8d420262186acecf541
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2019-06-24 12:00:00 -0700

    implement ReadData section

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,18 +6,23 @@
 using namespace Rcpp;
 
 // fit_network
-int fit_network();
-RcppExport SEXP _bayesnetworks_fit_network() {
+int fit_network(NumericMatrix X, IntegerVector Npar, IntegerVector nodetype, IntegerMatrix par, int MaxPar);
+RcppExport SEXP _bayesnetworks_fit_network(SEXP XSEXP, SEXP NparSEXP, SEXP nodetypeSEXP, SEXP parSEXP, SEXP MaxParSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    rcpp_result_gen = Rcpp::wrap(fit_network());
+    Rcpp::traits::input_parameter< NumericMatrix >::type X(XSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type Npar(NparSEXP);
+    Rcpp::traits::input_parameter< IntegerVector >::type nodetype(nodetypeSEXP);
+    Rcpp::traits::input_parameter< IntegerMatrix >::type par(parSEXP);
+    Rcpp::traits::input_parameter< int >::type MaxPar(MaxParSEXP);
+    rcpp_result_gen = Rcpp::wrap(fit_network(X, Npar, nodetype, par, MaxPar));
     return rcpp_result_gen;
 END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_bayesnetworks_fit_network", (DL_FUNC) &_bayesnetworks_fit_network, 0},
+    {"_bayesnetworks_fit_network", (DL_FUNC) &_bayesnetworks_fit_network, 5},
     {NULL, NULL, 0}
 };
 
--- src/fit_network.cpp
+++ src/fit_network.cpp
@@ -1,8 +1,37 @@
 #include <Rcpp.h>
+#include "cholesky21.h"
+#include "random4f.h"
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-int fit_network()
+int fit_network(NumericMatrix X,
+                IntegerVector Npar,
+                IntegerVector nodetype,
+                IntegerMatrix par,
+                int MaxPar)
 {
+  // Data
+  int N = X.nrow();
+  int P = X.ncol();
+
+  // Iterators
+  int p, e;
+
+  // Network
+  IntegerVector Nsimpar = Npar;
+  IntegerMatrix simpar = par;
+  IntegerMatrix simEdge(P, P);
+  int NsimEdges=0;
+
+  // ReadData
+  for (p=0; p<P; p++)
+  {
+    for (e=0; e<Npar[p]; e++)
+    {
+      simEdge(par(p, e), e) = 1;
+      NsimEdges ++;
+    }
+  }
+
   return 0;
 }
